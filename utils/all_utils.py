c763bf3f22393c987bda4867115afa8b56d1ffa0
linckon <linckon@live.com>
2022-02-12 02:31:22 +0600

    doc string added

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -5,6 +5,15 @@ import numpy as np
 import logging
 
 def prepare_data(df,target_col="y"):
+    """it return label and independent features
+
+    Args:
+        df ([pd.DataFrame]): This is a dataframe
+        target_col (str, optional): label col name. Defaults to "y".
+
+    Returns:
+        tuple: label and x
+    """
     logging.info("Preparing the data for training")
     X = df.drop(target_col,axis=1)
     
